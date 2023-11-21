d59fa6806de7129c733e6e575f041db44385150e
Kanji Wakabayashi <c0b2216365@edu.teu.ac.jp>
2023-11-21 16:46:14 +0900

    こうかとんがなめらか

--- ex01/pygame_image.py
+++ ex01/pygame_image.py
@@ -9,7 +9,7 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")            #imageモジュールのなかのload関数を利用して画像を読み込んでいる。戻り値はsurfaceクラス。練習1
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]  #こうかとんsurfaceのリスト。練習3
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0), pg.transform.rotozoom(kk_img, 12, 1.0), pg.transform.rotozoom(kk_img, 16, 1.0), pg.transform.rotozoom(kk_img, 12, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0), pg.transform.rotozoom(kk_img, 4, 1.0)]  #こうかとんsurfaceのリスト。練習3
     tmr = 0
     while True:                                         #無限ループ
         for event in pg.event.get():
@@ -18,10 +18,10 @@ def main():
         screen.blit(bg_img, [-x , 0]) 
         screen.blit(pg.transform.flip(bg_img, True, False), [1600 - x, 0])
         screen.blit(bg_img, (3200 - x, 0))
-        screen.blit(kk_imgs[tmr % 2], (300, 200))                        #bg_imgをblit(貼り付けて)いる。座標をべた書きすると移動しない。.get.
+        screen.blit(kk_imgs[tmr % 4], (300, 200))                        #bg_imgをblit(貼り付けて)いる。座標をべた書きすると移動しない。.get.
         pg.display.update()                                 #blitしたらapdateする。基本的にはwhile分の最後に入る
         tmr += 1        
-        clock.tick(300)
+        clock.tick(20)
 
 
 if __name__ == "__main__":
